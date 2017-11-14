63ade63893cfb5a1b9f223b6b497b64c6845b569
chenjiqiang <1399917041@qq.com>
2017-11-14 10:11:47 +0800

    add procedure rule--121 to rule--131

--- database/upgrade_package_sql.sql
+++ database/upgrade_package_sql.sql
@@ -3,6 +3,32 @@
   -- Created : 2017/11/3 17:15:27
   -- Purpose : Package for Antifraud
   -- 全局变量声明
+  --以下全局变量是r_121至r_131规则开发过程中添加的
+  --申请人姓名英文风险描述v_CODENAMEENGLISH = CODENAMEENGLISH
+  v_codenameenglish  af_request_applicantinfo.codenameenglish%type;
+  --申请人地址邮件英文风险描述v_codeemailenglish = codeemailenglish
+  v_codeemailenglish  af_request_applicantinfo.codeemailenglish%type;
+  --申请人地址英文风险描述v_codeaddrenglish  = ipenenglish
+  v_codeaddrenglish  af_request_applicantinfo.codeaddrenglish%type;
+  --申请人ip地址v_ipenenglish  = ipenenglish
+  v_ipenenglish  af_request_applicantinfo.ipenenglish%type;
+  --申请人国际移动用户识别码v_imsienenglish = imsienenglish
+  v_imsienenglish af_request_applicantinfo.imsienenglish%type;
+  --申请人国际移动设备标志v_imeienenglish = imeienenglish
+  v_imeienenglish af_request_applicantinfo.imeienenglish%type;
+  -- 申请人wifi的物理地址:v_wifimacenenglish = wifimacenenglish
+  v_wifimacenenglish af_request_applicantinfo.wifimacenenglish%type;
+  -- 是否有风险是否有风险，有风险为“T”，无风险为“F”:zm_isrisk=isrisk
+  zm_isrisk af_request_appinfo_zmivsinfo.isrisk%type;
+  -- 申请人出生日期:v_birth = c2_birth
+  v_birth af_request_applicantinfo.c2_birth%type;
+  -- 申请人证件有效日期:v_idte1 = c5_idte1
+  v_idte1 af_request_applicantinfo.c5_idte1%type;
+  -- 申请人证件起始日期:v_iddt1 = c2_iddt1
+  v_iddt1 af_request_applicantinfo.c2_iddt1%type;
+  -- 申请人证件类型:v_idtype = c1_idtype
+  v_idtype af_request_applicantinfo.c1_idtype%type;
+  --以上全局变量是r_121至r_131规则开发过程中添加的
   v_errors varchar2(500);
   -- 申请人证件号码
   v_idnbr af_request_applicantinfo.c1_idnbr%type;
@@ -50,9 +76,13 @@ create or replace package body AF_HXBCB is
         c4_abuser,
         c4_abname,
         c1_idnbr,
-        c1_coname into v_coadd, v_hmadd, v_mobile, v_cotel, v_hmtel, v_abuser, v_abname, v_idnbr, v_coname
+        c1_coname,c5_idte1,c2_iddt1,c1_idtype,c2_birth,wifimacenenglish,imeienenglish,imsienenglish,ipenenglish,codeaddrenglish,codeemailenglish,codenameenglish into v_coadd, v_hmadd, v_mobile, v_cotel, v_hmtel, v_abuser, v_abname, v_idnbr, v_coname,v_idte1,v_iddt1,v_idtype,
+        v_birth,v_wifimacenenglish,v_imeienenglish,v_imsienenglish,v_ipenenglish,v_codeaddrenglish,v_codeemailenglish,v_codenameenglish
       from af_request_applicantinfo
       where app_id = app_id_input;
+
+      --从af_request_appinfo_zmivsinfo获取全局变量
+      select isrisk into zm_isrisk from af_request_appinfo_zmivsinfo t  where t.app_id = app_id_input AND isrisk='T';
       --调用规则
       -- TODO业务场景是(需要添加流程控制):
       -- 1.申请件首次进来会直接进行黑名单规则计算,不执行模型和SNA计算
@@ -85,6 +115,16 @@ create or replace package body AF_HXBCB is
   procedure AF_HXBCB_RULE(app_id_input in varchar2) is
     rules_riskcode af_response_afriskwarning.riskcode%type;
     begin
+      AF_HXBCB_RULE_PKG.RULE_131(app_id_input,v_codenameenglish);
+      AF_HXBCB_RULE_PKG.RULE_130(app_id_input,v_codeemailenglish);
+      AF_HXBCB_RULE_PKG.RULE_129(app_id_input,v_codeaddrenglish);
+      AF_HXBCB_RULE_PKG.RULE_128(app_id_input,v_ipenenglish);
+      AF_HXBCB_RULE_PKG.RULE_127(app_id_input,v_imsienenglish);
+      AF_HXBCB_RULE_PKG.RULE_126(app_id_input,v_imeienenglish);
+      AF_HXBCB_RULE_PKG.RULE_125(app_id_input,v_wifimacenenglish);
+      AF_HXBCB_RULE_PKG.RULE_124(app_id_input,zm_isrisk);
+      AF_HXBCB_RULE_PKG.RULE_122(app_id_input,v_idte1,v_iddt1);
+      AF_HXBCB_RULE_PKG.RULE_121(app_id_input,v_idtype, v_idnbr,v_iddt1,v_idte1,v_birth);
       --调用规则包中的黑名单规则编号160
       AF_HXBCB_RULE_PKG.RULE_155(app_id_input, v_idnbr);
       AF_HXBCB_RULE_PKG.RULE_158(app_id_input, v_coname);
